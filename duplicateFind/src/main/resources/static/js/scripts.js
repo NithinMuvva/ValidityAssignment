8863ea3b0b9ed2913123d40e4605fb8c92274fb4
Nithinmuvva <muvva.n@husky.neu.edu>
2019-05-18 02:54:29 -0400

    similarRecords functionality UI

---
+++ duplicateFind/src/main/resources/static/js/scripts.js
@@ -0,0 +1,135 @@
+
+function myFunction(){
+    var file = document.getElementById('myFile').files[0];
+    var txt = "";
+    if (file) {
+        txt += "name: " + file.name + "<br>";
+        getTableData(file);
+        //chartFunction(45,55);
+        $("#selection").hide();
+    } 
+    else {
+    	txt += "Select a file.";
+    }
+    $("#demo").val(txt);
+}
+
+function getTableData(){
+  $('#duplicateRecords > tbody > tr:nth-child(n+2)').remove();
+  $('#nonSimilarRecords > tbody > tr:nth-child(n+2)').remove();
+  var form = document.forms[0];
+  var formData = new FormData(form);
+  $.ajax({
+   type: "POST",
+   url: "/getSimilarRecords",
+   contentType: "application/json",
+   dataType : "json",
+   data : formData,
+   cache : false,
+   contentType : false,
+   processData : false,
+   success: function (data) {
+   var similarData = data['similarData'];
+   var uniqueData = data['uniqueData'];
+   $.each(similarData,function(i, item) {
+        var rowdata = '<tr> '
+            +'<td>'
+            + similarData[i].first_name
+            +'</td>'
+            + '<td>'
+            + similarData[i].last_name
+            + '</td>'
+            + '<td>'
+            + similarData[i].company
+            + '</td>'
+            + '<td>'
+            + similarData[i].email
+            + '</td>'
+            + '<td>'
+            + similarData[i].address
+            + '</td>'
+            + '<td>'
+            + similarData[i].zip
+            + '</td>'
+            + '<td>'
+            +similarData[i].city
+            + '</td>'
+            + '<td>'
+            +similarData[i].state
+            + '</td>'
+            + '<td>'
+            +similarData[i].phone
+            + '</td>'
+            + '</tr>';
+            $("#duplicateRecords").append(rowdata);
+   });
+   $.each(uniqueData,function(i, item) {
+        var rowdata = '<tr> '
+            +'<td>'
+            + uniqueData[i].first_name
+            +'</td>'
+            + '<td>'
+            + uniqueData[i].last_name
+            + '</td>'
+            + '<td>'
+            + uniqueData[i].company
+            + '</td>'
+            + '<td>'
+            + uniqueData[i].email
+            + '</td>'
+            + '<td>'
+            + uniqueData[i].address
+            + '</td>'
+            + '<td>'
+            + uniqueData[i].zip
+            + '</td>'
+            + '<td>'
+            +uniqueData[i].city
+            + '</td>'
+            + '<td>'
+            +uniqueData[i].state
+            + '</td>'
+            + '<td>'
+            +uniqueData[i].phone
+            + '</td>'
+            + '</tr>';
+            $("#nonSimilarRecords").append(rowdata);
+   	});
+	   $("#duplicateTable").show();
+	   $("#nonSimilarTable").show();
+	   var similarLength = (similarData.length/(similarData.length+uniqueData.length))*100;
+	   var uniqueLength = (uniqueData.length/(similarData.length+uniqueData.length))*100;
+	   chartFunction(similarLength,uniqueLength);
+   },
+   error : function(data,errorThrown){
+        console.log(errorThrown);
+   }
+   });   
+}
+
+function chartFunction(x,z){
+console.log('in chartFunction');
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+        title:{
+            text: "Data Analysis"
+        },
+        legend: {
+            maxWidth: 350,
+            itemWidth: 120
+        },
+        data: [
+        {
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "#percent %",
+            legendText: "{indexLabel}",
+            dataPoints: [
+                { y: x, indexLabel: "Similar Records" },
+                { y: z, indexLabel: "Unique Records" }
+            ]
+        }
+        ]
+    });
+    chart.render();
+}
---
+++ duplicateFind/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,56 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript" src="/resources/js/scripts.js"></script>
+</head>
+<body>
+<form action="fileUpload" method="post" enctype="multipart/form-data">
+	<div  class="form-group" id="selection">
+	    <h3>Upload csv file for duplicate records</h3>
+	    <div>
+	        <input type="file" id="myFile" size="50" name="file" accept = ".csv" onchange= "myFunction()">
+	        <p id="demo"></p>
+	    </div>
+	</div> 
+</form>
+<div id="chartContainer" style="height: 300px; width: 100%;"></div><br>
+<div id="duplicateTable" style="display:none;">
+    <h4>Duplicate Records</h4>
+    <table id="duplicateRecords">
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Company</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Zip</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Phone</th>
+        </tr>
+    </table>
+</div>
+<div id="nonSimilarTable" style="display:none;">
+        <h4>Unique Records</h4>
+        <table id="nonSimilarRecords">
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Company</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Zip</th>
+                <th>City</th>
+                <th>State</th>
+                <th>Phone</th>
+            </tr>
+        </table>
+    </div>
+
+</body>
+</html>
